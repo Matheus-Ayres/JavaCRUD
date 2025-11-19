799adc8729903f733695e2b578c354bd63c18eee
Matheus-Ayres <matheussayres9@gmail.com>
2025-11-19 13:20:30 -0300

    ✅

--- Products/src/App.java
+++ Products/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Service service = new Service();
+
+        service.createProduct(1, "car", 10);
+        service.createProduct(2, "moto", 20);
+
+        System.out.println("Products list:");
+        service.listProducts();
+
+        service.updateProductName(1, "carro");
+
+        service.searchProduct(2);
+
+        service.updateProductPrice(2, 30);
+
+        service.listProducts();
+
+        service.deleteProduct(2);
+        service.listProducts();
+
     }
 }
