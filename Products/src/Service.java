881aa0668c3bd5c9ceaeb32444d9aeb95f2914fd
Matheus-Ayres <matheussayres9@gmail.com>
2025-11-19 14:17:27 -0300

    Finished :rocket:

--- Products/src/Service.java
+++ Products/src/Service.java
@@ -1,63 +1,49 @@
 public class Service {
     
     public void createProduct(int id, String name, int price){
-        if(FakeDB.searchProduct(id) != null){
-            System.out.println("ID already exists!");
-            return;
-        }
-
         Product novo = new Product(id, name, price);
         FakeDB.saveProduct(novo);
     }
 
     public void listProducts(){
-        for (Product p : FakeDB.allProducts()){
-            System.out.println(p.getId() + " - " + p.getName() + " R$" + p.getPrice());
-        }
+        for (Product product : FakeDB.allProducts()){
+            System.out.println( product.getId() + " - " + product.getName() + " R$" + product.getPrice() );
+        } 
     }
 
     public Product searchProduct(int id){
-        return FakeDB.searchProduct(id);
+        Product product = FakeDB.searchProduct(id);
+    if(product != null){
+        System.out.println(product.getId() + " - " + product.getName() + " R$" + product.getPrice());
+    } else {
+        System.out.println("Product not found");
+    }
+    return product;
+
     }
 
     public void updateProductName(int id, String newName){
-        Product p = FakeDB.searchProduct(id);
-
-        if(p == null){
+        Product product = FakeDB.searchProduct(id);
+        if(product != null){
+            product.setProductName(newName);
+        }else{
             System.out.println("Product not found");
-            return;
         }
-
-        p.setProductName(newName);
-        System.out.println("Name updated.");
     }
 
     public void updateProductPrice(int id, int newPrice){
-        if(newPrice < 0){
-            System.out.println("Invalid price");
-            return;
-        }
-
-        Product p = FakeDB.searchProduct(id);
-
-        if(p == null){
+        Product product = FakeDB.searchProduct(id);
+        if(product != null){
+            product.setProductPrice(newPrice);
+        }else{
             System.out.println("Product not found");
-            return;
         }
-
-        p.setProductPrice(newPrice);
-        System.out.println("Price updated.");
     }
 
     public void deleteProduct(int id){
-        Product p = FakeDB.searchProduct(id);
-
-        if(p == null){
-            System.out.println("Product not found");
-            return;
+        Product product = FakeDB.searchProduct(id);
+        if(product != null){
+            FakeDB.delete(product);
         }
-
-        FakeDB.delete(p);
-        System.out.println("Product deleted.");
     }
 }
